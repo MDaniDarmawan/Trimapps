900bc25cb5f63a7bb5bbb4d0721ee9632909a7da
MDaniDarmawan <darmawandani1922@gmail.com>
2023-10-26 08:32:56 +0700

    update login dan register

--- app/src/main/java/com/example/trimapps/LoginActivity.kt
+++ app/src/main/java/com/example/trimapps/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.trimapps
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -14,29 +15,62 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: LoginActivityBinding
 
-    lateinit var email : EditText
-    lateinit var pass: EditText
-    lateinit var button: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = LoginActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        emailFocusListener()
+        passwordFocusListener()
+    }
 
-        email = binding.etEmail
-        pass = binding.etPassword
-        button = binding.button
+    private fun emailFocusListener()
+    {
+        binding.etEmail.setOnFocusChangeListener { _, focused ->
+            if (!focused)
+            {
+        binding.tilEmail.helperText = validEmail()
+    }
+}
+}
+        private fun validEmail(): String?
+        {
+        val emailText = binding.etEmail.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches())
+        {
+            return "Email kurang lengkap"
+    }
+        return null
+    }
 
-        binding.button.setOnClickListener(View.OnClickListener {
-            if (email.text.toString() == "user" && pass.text.toString() == "1234") {
-                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
-
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+    private fun passwordFocusListener()
+    {
+        binding.etKataSandi.setOnFocusChangeListener{ _, focused ->
+            if (!focused)
+            {
+                binding.tilKataSandi.helperText = validPassword()
             }
-        })
+        }
+    }
+    private fun validPassword(): String?
+    {
+        val passwordText = binding.etKataSandi.text.toString()
+        if (passwordText.length < 8)
+        {
+            return "Kata sandi minimal 8 karakter"
+        }
+        if (!passwordText.matches(".*[A-Z].*".toRegex()))
+        {
+            return "Kata sandi harus mengandung 1 huruf kapital"
+        }
+        if (!passwordText.matches(".*[a-z].*".toRegex()))
+        {
+            return "Kata sandi harus mengandung 1 huruf kecil"
+        }
+        if (!passwordText.matches(".*[@#\$%^&+=].*".toRegex()))
+        {
+            return "Kata sandi harus mengandung 1 spesial karakter (@#\$%^&+=)"
+        }
+        return null
     }
 }
