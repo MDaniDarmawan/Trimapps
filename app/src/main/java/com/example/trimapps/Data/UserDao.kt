eb995d527e4e73b539ecd78ea459a35ad883d50e
mhvanreal <hilmyofficial03@gmail.com>
2023-10-26 10:17:53 +0700

    buat user database

--- app/src/main/java/com/example/trimapps/Data/UserDao.kt
+++ app/src/main/java/com/example/trimapps/Data/UserDao.kt
@@ -10,9 +10,15 @@ public interface UserDao {
    //@Insert
    //void registerUser(userEntity: UserEntity?)
 
+
+    // Metode ini digunakan untuk melakukan pencarian pengguna berdasarkan nama pengguna (userId) dan kata sandi (password)
+    // saat proses login dan autentikasi.
     @Query("SELECT * from user where usernameApp = :userId and password = :password")
     fun login(userId: String?, password: String?): UserEntity?
 
+    // Metode ini digunakan untuk mencari pengguna berdasarkan nama pengguna (username).
+    @Query("SELECT * from user where usernameApp = :username")
+    fun getUserByUsername(username: String?): UserEntity?
 
 
 }
