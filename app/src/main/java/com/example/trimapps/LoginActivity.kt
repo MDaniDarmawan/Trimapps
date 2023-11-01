bc93f729b9d84cd24ece8ec392b4e2f608a84b7c
MDaniDarmawan <darmawandani1922@gmail.com>
2023-11-01 11:38:27 +0700

    update fragment login dan register

--- app/src/main/java/com/example/trimapps/LoginActivity.kt
+++ app/src/main/java/com/example/trimapps/LoginActivity.kt
@@ -1,123 +1,48 @@
 package com.example.trimapps
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Selection
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.TextPaint
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.example.trimapps.Database.UserDao
-import com.example.trimapps.Database.UserRoomDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trimapps.MainActivity
+import com.example.trimapps.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
-    private lateinit var registerText: TextView
-
-    private lateinit var database: UserRoomDatabase
-    private lateinit var userDao: UserDao
+    private lateinit var registerTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_login)
 
-        database = UserRoomDatabase.getDatabase(applicationContext)
-        userDao = database.UserDao()
-
-        emailEditText = findViewById(R.id.til_email)
-        passwordEditText = findViewById(R.id.til_kata_sandi)
+        // Inisialisasi elemen UI
+        emailEditText = findViewById(R.id.et_email)
+        passwordEditText = findViewById(R.id.et_kata_sandi)
         loginButton = findViewById(R.id.button_masuk)
-        registerText = findViewById(R.id.daftar)
+        registerTextView = findViewById(R.id.daftar)
 
-        loginButton.setOnClickListener {
-            val username = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
+        // Tambahkan event listener untuk tombol login
+        loginButton.setOnClickListener(View.OnClickListener {
+            if (emailEditText.text.toString() == "user" && passwordEditText.text.toString() == "1234") {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
 
-            CoroutineScope(Dispatchers.IO).launch {
-                val user = userDao.getUser(username, password)
-                val email = userDao.getEmail(username)
-                withContext(Dispatchers.Main) {
-                    if (user != null) {
-                        Toast.makeText(applicationContext, "Login Berhasil", Toast.LENGTH_SHORT).show()
-                        SharedPreferencesUtil.saveLoggedInUser(this@LoginActivity, username, email)
-                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        Toast.makeText(applicationContext, "Username atau password salah", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-        registerText.makeLinks(
-            Pair("Daftar sekarang!", View.OnClickListener {
-                val intent = Intent(this, RegisterActivity::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
-            }))
-    }
-
-    private fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
-        val spannableString = SpannableString(this.text)
-        var startIndexOfLink = -1
-        for (link in links) {
-            val clickableSpan = object : ClickableSpan() {
-                override fun updateDrawState(textPaint: TextPaint) {
-                    // use this to change the link color
-                    textPaint.color = textPaint.linkColor
-                    // toggle below value to enable/disable
-                    // the underline shown below the clickable text
-                    textPaint.isUnderlineText = true
-                }
-
-                override fun onClick(view: View) {
-                    Selection.setSelection((view as TextView).text as Spannable, 0)
-                    view.invalidate()
-                    link.second.onClick(view)
-                }
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
             }
-            startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
-//      if(startIndexOfLink == -1) continue // todo if you want to verify your texts contains links text
-            spannableString.setSpan(
-                clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-        }
-        this.movementMethod =
-            LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
-        this.setText(spannableString, TextView.BufferType.SPANNABLE)
-    }
-    object SharedPreferencesUtil {
-        fun saveLoggedInUser(context: Context, username: String, email: String) {
-            val sharedPreferences = context.getSharedPreferences("UserData", Context.MODE_PRIVATE)
-            val editor = sharedPreferences.edit()
-            editor.putString("userName", username)
-            editor.putString("userEmail", email)
-            editor.apply()
-        }
-
-        fun getLoggedInUser(context: Context): Pair<String?, String?> {
-            val sharedPreferences = context.getSharedPreferences("UserData", Context.MODE_PRIVATE)
-            val userName = sharedPreferences.getString("userName", null)
-            val userEmail = sharedPreferences.getString("userEmail", null)
-
-            return Pair(userName, userEmail)
+        })
+        registerTextView.setOnClickListener {
+            // Arahkan ke halaman pendaftaran
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
-
 }
--- app/src/main/java/com/example/trimapps/RegisterActivity.kt
+++ app/src/main/java/com/example/trimapps/RegisterActivity.kt
@@ -2,115 +2,58 @@ package com.example.trimapps
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Selection
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.TextPaint
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
+import android.util.Patterns
 import android.view.View
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.trimapps.Database.User
-import com.example.trimapps.Database.UserRoomDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.example.trimapps.databinding.ActivityRegisterBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
-
     private lateinit var usernameEditText: EditText
-    private lateinit var passwordEditText: EditText
-    private lateinit var usergithubEditText: EditText
     private lateinit var emailEditText: EditText
+    private lateinit var githubUsernameEditText: EditText
+    private lateinit var passwordEditText: EditText
     private lateinit var registerButton: Button
-    private lateinit var loginText: TextView
+    private lateinit var loginTextView: TextView
 
+    private lateinit var mBinding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_activity)
+        setContentView(R.layout.activity_register)
 
-        val database = UserRoomDatabase.getDatabase(applicationContext)
-        val userDao = database.UserDao()
-
-        usernameEditText = findViewById(R.id.til_nama_pengguna)
-        passwordEditText = findViewById(R.id.til_kata_sandi)
-        usergithubEditText = findViewById(R.id.til_github)
-        emailEditText = findViewById(R.id.til_email)
+        // Inisialisasi elemen UI
+        usernameEditText = findViewById(R.id.et_nama_pengguna)
+        emailEditText = findViewById(R.id.et_email)
+        githubUsernameEditText = findViewById(R.id.et_github)
+        passwordEditText = findViewById(R.id.et_kata_sandi)
         registerButton = findViewById(R.id.button_daftar)
-        loginText = findViewById(R.id.masuk)
+        loginTextView = findViewById(R.id.masuk)
 
-        loginText.makeLinks(
-            Pair("Masuk sekarang!", View.OnClickListener {
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-            }))
-
-        registerButton.setOnClickListener {
-            val username = usernameEditText.text.toString()
-            val password = passwordEditText.text.toString()
-            val usergithub = usergithubEditText.text.toString()
-            val email = emailEditText.text.toString()
-
-
-            if (username.isNotEmpty() && password.isNotEmpty() && usergithub.isNotEmpty() && email.isNotEmpty()) {
-                // Check if the username is already in use
-                CoroutineScope(Dispatchers.IO).launch {
-                    val existingUser = userDao.getUserByUsername(username)
-                    if (existingUser == null) {
-                        // User doesn't exist, proceed with registration
-                        val newUser = User(userApp = username, password = password, userGithub = usergithub, email = email)
-                        userDao.insert(newUser)
-                        runOnUiThread {
-                            Toast.makeText(this@RegisterActivity, "Data berhasil disimpan, silahkan kembali", Toast.LENGTH_SHORT).show()
+                    // Tambahkan event listener untuk tombol register
+                    registerButton.setOnClickListener {
+                        val username = usernameEditText.text.toString()
+                        val email = emailEditText.text.toString()
+                        val githubUsername = githubUsernameEditText.text.toString()
+                        val password = passwordEditText.text.toString()
+                        if (username.isNotEmpty() && email.isNotEmpty() && githubUsername.isNotEmpty() && password.isNotEmpty()) {
+                            Toast.makeText(this, "Pendaftaran Berhasil!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "Pendaftaran Gagal!", Toast.LENGTH_SHORT).show()
                         }
-                        // Optionally, you can navigate to the login page or perform other actions.
-                    } else {
-                        // Username already in use, show an error message.
-                        runOnUiThread {
-                            Toast.makeText(this@RegisterActivity, "Username telah digunakan", Toast.LENGTH_SHORT).show()
-                        }
-                        // You may want to use runOnUiThread or LiveData to update the UI.
+
+                        // Lakukan validasi data jika diperlukan
+
+                        // Lakukan operasi pendaftaran atau penyimpanan data ke server atau database
                     }
-                }
-            } else {
-                Toast.makeText(this, "Harap isi semua kolom yang ada", Toast.LENGTH_SHORT).show()
-            }
-        }
+                            loginTextView.setOnClickListener {
+                        // Arahkan ke halaman pendaftaran
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
     }
-}
-
-private fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
-    val spannableString = SpannableString(this.text)
-    var startIndexOfLink = -1
-    for (link in links) {
-        val clickableSpan = object : ClickableSpan() {
-            override fun updateDrawState(textPaint: TextPaint) {
-                // use this to change the link color
-                textPaint.color = textPaint.linkColor
-                // toggle below value to enable/disable
-                // the underline shown below the clickable text
-                textPaint.isUnderlineText = true
-            }
-
-            override fun onClick(view: View) {
-                Selection.setSelection((view as TextView).text as Spannable, 0)
-                view.invalidate()
-                link.second.onClick(view)
-            }
-        }
-        startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
-//      if(startIndexOfLink == -1) continue // todo if you want to verify your texts contains links text
-        spannableString.setSpan(
-            clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-    }
-    this.movementMethod =
-        LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
-    this.setText(spannableString, TextView.BufferType.SPANNABLE)
 }
